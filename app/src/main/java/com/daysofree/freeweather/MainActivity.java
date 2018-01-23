31ed8a3937af238f686b05f91b1177eb8f1be380
tianyi <tianyi@gshopper.com>
2018-01-23 15:52:23 +0800

    当用户选择过城市后,下次启动应用直接跳到该城市天气页面而不用再次选择城市

--- app/src/main/java/com/daysofree/freeweather/MainActivity.java
+++ app/src/main/java/com/daysofree/freeweather/MainActivity.java
@@ -1,7 +1,12 @@
 package com.daysofree.freeweather;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String weatherVal = prefs.getString("weather", "");
+        if (!TextUtils.isEmpty(weatherVal)) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), "欢迎使用免费天气应用", Toast.LENGTH_LONG).show();
+        }
     }
+
 }
